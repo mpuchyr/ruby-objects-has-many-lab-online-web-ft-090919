4f5806bd33200236885e47bf1d097a75518b5d4b
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-18 00:23:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
   
   @@all = []
   
@@ -11,4 +11,9 @@ class Song
   def self.all
     @@all
   end
+  
+  def artist_name
+    self.artist.name
+  end
+  
 end
